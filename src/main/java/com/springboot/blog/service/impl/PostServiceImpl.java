c0df4664b396105bfa33769d85ab0056294faa3a
ashishkempwad <ashish.kempwad@alumini.iiit.ac.in>
2024-04-09 21:05:03 +0530

    created findall and findbyId apis

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -1,12 +1,16 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -19,29 +23,65 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional
     public PostDto createPost(PostDto postDto) {
 
-        //convert dto to entity
+        //TODO: Add exception handling. When query fails it should rollback and ID should not be incremented
+        try {
+            //convert dto to entity
+            Post post = mapToEntity(postDto);
+
+            //Getting response from the repository
+            Post newPost = postRepository.save(post);
+
+            //convert entity to dto
+            return  mapToDto(newPost);
+
+        }catch(DataIntegrityViolationException e){
+            throw new IllegalArgumentException("Title must be unique",e);
+
+        }catch(Exception ex){
+            throw new RuntimeException("Failed to create the post", ex);
+        }
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+
+        //Get all the posts from the database
+        List<Post> posts = postRepository.findAll();
+
+        //Now we need to map all the posts to dto and return
+        return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post= postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post","id",id));
+        return mapToDto(post);
+    }
+
+    //convert dto to entity
+    private Post mapToEntity(PostDto postDto){
+
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
 
-        //Getting response from the repository
-        Post newPost = postRepository.save(post);
-
-        //convert entity to dto
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return post;
     }
 
-    @Override
-    public List<PostDto> getAllPosts() {
-        return null;
+    //convert entity to dto
+    private PostDto mapToDto(Post post){
+
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
     }
 }
